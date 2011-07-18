77f37231af916d9c3a777b7fc077c7241aa464f9
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-07-18 18:02:50 +0900

    Add simple archive script and schedule it

---
+++ cron/archive.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VCDDATA=/opt/vmware/vcdkit/data
+
+for d in vcd-dump vcd-report
+do
+  for t in `find $VCDDATA/$d -maxdepth 1 -mindepth 1 -mtime +1 -type d`
+  do
+    file=`basename $t`
+    dir=`dirname $t`
+    echo "Creating tar archive: $t"
+      tar zcf $dir/$file.tgz -C $dir $file && rm -fr $t
+  done
+done
