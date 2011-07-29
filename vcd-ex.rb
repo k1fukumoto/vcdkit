4e38b17a86c33d1001e6804dbba79970d022e912
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-07-29 19:00:47 +0900

    Add multi env support

--- vcd-ex.rb
+++ vcd-ex.rb
@@ -27,7 +27,14 @@ optparse = OptionParser.new do |opt|
   opt.banner = "Usage: vcd-ex.rb [options]"
 
   opt.on('-v','--vcd HOST,ORG,USER',Array,'vCD login parameters') do |o|
-    options[:vcd] = o
+    case o[0]
+    when "1"
+      options[:vcd] = vcd1
+    when "2"
+      options[:vcd] = vcd2
+    else
+      options[:vcd] = o
+    end
   end
 
   opt.on('-l','--logfile LOGFILEPATH','Log file name') do |o|
@@ -61,22 +68,18 @@ end
 
 VCDEX_DIR   = './data/vcd-ex'
 VCDEX_ORG   = 'Admin'
-VCDEX_JOBS  = 
-  [
-   { :vcd => vcd2,
-     :vapps => [
-                Target.new('Admin','VCDEX-01'),
-                ]
-   },
-   { :vcd => vcd1,
-     :vapps => [
-                Target.new('Basic Backup - Admin','VCDEX-BB01'),
-                Target.new('Basic - Admin','VCDEX-B01'),
-                Target.new('Committed Backup - Admin','VCDEX-CB01'),
-                Target.new('Committed - Admin','VCDEX-C01'),
-               ]
-   },
-]
+VCDEX_JOBS  = {
+  vcd2 => [
+           Target.new('Basic Backup - Admin','VCDEX-BB01'),
+           Target.new('Committed Backup - Admin','VCDEX-CB01'),
+          ],
+  vcd1 => [
+           Target.new('Basic Backup - Admin','VCDEX-BB01'),
+           Target.new('Committed Backup - Admin','VCDEX-CB01'),
+           Target.new('Basic - Admin','VCDEX-B01'),
+           Target.new('Committed - Admin','VCDEX-C01'),
+          ],
+}
 
 #
 # MAIN
@@ -84,40 +87,40 @@ VCDEX_JOBS  =
 $log = VCloud::Logger.new(options[:logfile])
 FileUtils.mkdir_p(VCDEX_DIR) unless File.exists? VCDEX_DIR
 
+pp options
 begin
-  VCDEX_JOBS.each do |job|
-    vcd = VCloud::VCD.new
-    vcd.connect(*job[:vcd])
-    org = vcd.org(VCDEX_ORG)
+  vcd = VCloud::VCD.new
+  vcd.connect(*options[:vcd])
+  org = vcd.org(VCDEX_ORG)
 
-    # Get thumbnails from all ESX hosts
-    job[:vapps].each do |t|
-      vapp = org.vdc(t.vdc).vapp(t.vapp)
+  # Get thumbnails from all ESX hosts
+  VCDEX_JOBS[options[:vcd]].each do |t|
+    vapp = org.vdc(t.vdc).vapp(t.vapp)
 
-      if(vapp.status == "Powered Off")
-        vcd.wait(vapp.powerOn)
-      end
-
-      vapp.each_vm do |vm|
-        open("#{VCDEX_DIR}/#{vm.name}.png",'w') do |f|
-          f.write vm.thumbnail
-        end
-      end
+    if(vapp.status == "Powered Off")
+      vcd.wait(vapp.powerOn)
     end
 
-    # Recycle power of one of vApp
-    t = job[:vapps][0]
-    vdc = org.vdc(t.vdc)
-
-    vapp = vdc.vapp(t.vapp)
-    vcd.wait(vapp.powerOff)
-
-    vapp = vdc.vapp(t.vapp)
-    vcd.wait(vapp.undeploy)
-
-    vapp = vdc.vapp(t.vapp)
-    vcd.wait(vapp.powerOn)
+    vapp.each_vm do |vm|
+      open("#{VCDEX_DIR}/#{vm.name}.png",'w') do |f|
+        f.write vm.thumbnail
+      end
+    end
   end
+
+  # Recycle power of one of vApp
+  t = VCDEX_JOBS[options[:vcd]][0]
+  vdc = org.vdc(t.vdc)
+
+  vapp = vdc.vapp(t.vapp)
+  vcd.wait(vapp.powerOff)
+
+  vapp = vdc.vapp(t.vapp)
+  vcd.wait(vapp.undeploy)
+
+  vapp = vdc.vapp(t.vapp)
+  vcd.wait(vapp.powerOn)
+
 rescue Exception => e
   $log.error("vcd-ex failed: #{e}")
   $log.error(e.backtrace)
