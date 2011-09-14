d272e76896854480a67a75fbba50198a05cb37aa
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-09-14 13:54:16 +0900

    Start adding end-to-end restore test

--- test/restore.sh
+++ test/restore.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-time ( \
-    rm -frv data/vcd-report/RESTORE && \
-    ./vcd-restore.rb -v2 -tBASE -idata/vcd-dump -odata/vcd-report -aVMTest,'Basic - VMTest',BACKUPTEST-01 
-)
+rm -frv data/* logs/restore.log && \
+  ./vcd-dump.rb -v3 -t RESTORETEST -l logs/restore.log -V && \
+  ./vcd-report.rb -s -l logs/restore.log -V && \
+  ./vcd-restore.rb -s -v3 -tRESTORETEST -l logs/restore.log -V \
+    -aAdmin,Admin,RESTORETEST01 && \
+  ! grep -e WARN -e ERROR logs/restore.log && 
+
+echo TEST RESULT: $?
+
