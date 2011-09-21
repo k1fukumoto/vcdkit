a0f3b313d78f79c49d4a64668706314bc8ac54a4
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-09-22 08:26:12 +0900

    Fix typo

--- test/restore.sh
+++ test/restore.sh
@@ -8,7 +8,7 @@ VAPP=-aAdmin,Admin,RESTORETEST01
   ./vcd-dump.rb $VCD $VC -t RESTORETEST -l logs/restore.log -V && \
   ./vcd-report.rb -s -l logs/restore.log -V && \
   ./vcd-restore.rb -s $VCD -tRESTORETEST -l logs/restore.log -V $VAPP && \
-  ! grep -e WARN -e ERROR logs/restore.log && 
+  ! grep -e WARN -e ERROR logs/restore.log 
 
 echo TEST RESULT: $?
 
