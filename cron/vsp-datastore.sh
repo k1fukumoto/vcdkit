6c1abc2311db3fa26b93a0eb961dae44b72de60a
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-10-04 16:56:26 +0900

    Fix typo

--- cron/vsp-datastore.sh
+++ cron/vsp-datastore.sh
@@ -2,5 +2,5 @@
 
 export VCDKIT=/opt/vmware/vcdkit
 
-$VCDKIT/vsp-datastore.rb -c2 -l$VCDKIT/logs/vcd-dump.log -C $VCDKIT/conf/vsp-datastore.xml -D -t -m $VCDKIT/conf/mail/vsp-datastore.xml >/dev/null
+$VCDKIT/vsp-datastore.rb -c2 -l$VCDKIT/logs/vsp-datastore.log -C $VCDKIT/conf/vsp-datastore.xml -D -t -m $VCDKIT/conf/mail/vsp-datastore.xml >/dev/null
 
