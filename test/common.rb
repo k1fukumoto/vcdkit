61b4e01ac007c7c6f89f2c692e6298cb45580b33
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-10-12 14:09:47 +0900

    Define command options in an external XML conf file

--- test/common.rb
+++ test/common.rb
@@ -1,3 +1,4 @@
+require 'rexml/document'
 require 'rake/clean'
 
 VCDKIT=ENV['VCDKIT']
@@ -7,26 +8,27 @@ CLEAN.include("#{VCDKIT}/data/*",LOG,'**/*.done')
 # Verbose shell command execution
 verbose(true)
 
+CONF = REXML::Document.new(File.new("#{VCDKIT}/conf/test.xml"))
 
 class Target
   attr_reader :name,:command,:done
   attr_accessor :repeat
 
-  OPTIONS = {
-    :log => "-l#{LOG}",
-    :vcd => '-vtvcd.vcdc.whitecloud.jp,System,vcdadminl',
-    :exops => '-T -P',
-    :vc => '',
-    :org => '-aAdmin',
-    :vapp => '-aAdmin,\'Committed Backup - Admin\',RESTORETEST01',
-    :tree => '--tree TESTALL',
-  }
-
   def initialize(name,params={})
     @name = name
     @done = "#{name}.done"
     @repeat = params[:repeat] || 1
-    opts = (params[:opts] || []).collect {|o| (o.class == Symbol)? OPTIONS[o] : o}
+    opts = (params[:opts] || []).collect {|o| 
+      if (o.class == Symbol)
+        e = CONF.root.elements["//cmdopt[@name='#{o}']"]
+        if(e.nil?)
+          raise "Cannot find command option: '#{o}'"
+        end
+        e.text
+      else
+        o
+      end
+    }
     @command = "#{VCDKIT}/#{name}.rb #{opts.join(' ')}"
   end
 
