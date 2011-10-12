1d7e2ef4b74c90582b4b43a9f89366110435ea02
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-10-12 14:10:14 +0900

    -s option is no longer available

--- test/restore/Rakefile.rb
+++ test/restore/Rakefile.rb
@@ -1,5 +1,5 @@
 require '../common'
 
 Target.setup(Target.new('vcd-dump',:opts => [:vcd,:vc,:tree,:org,:log]),
-             Target.new('vcd-report',:opts => ['-s',:vapp,:log]),
+             Target.new('vcd-report',:opts => [:vapp,:log]),
              Target.new('vcd-restore',:opts => ['-s',:vcd,:tree,:vapp,:log]))
