d14113cfb8182bf5467de05eefe584c8e3d30c65
Kaoru Fukumoto <kfukumot@vmware.com>
2011-10-20 17:23:08 +0900

    Supress stdout from cron script

--- cron/vcd-vapp.sh
+++ cron/vcd-vapp.sh
@@ -4,7 +4,7 @@ export VCDKIT=/opt/vmware/vcdkit
 
 $VCDKIT/vcd-vapp.rb -v2 \
   -D --vdc Admin,'Committed Backup - Admin', -nCBMON \
-  -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vcd-vapp.xml && \
+  -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vcd-vapp.xml > /dev/null && \
 $VCDKIT/vcd-vapp.rb -v2 \
   -A --vapptemplate Admin,'Committed Backup - Admin',CBMON -nCBMON  \
   -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vcd-vapp.xml > /dev/null
