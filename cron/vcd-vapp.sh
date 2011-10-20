f31adf6f6b9ee1b172561ace8175d544f2af6292
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-10-20 09:36:48 +0900

    Use command specific mail configuration

--- cron/vcd-vapp.sh
+++ cron/vcd-vapp.sh
@@ -4,8 +4,8 @@ export VCDKIT=/opt/vmware/vcdkit
 
 $VCDKIT/vcd-vapp.rb -v2 \
   -D --vdc Admin,'Committed Backup - Admin', -nCBMON \
-  -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vsp-datastore.xml && \
+  -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vcd-vapp.xml && \
 $VCDKIT/vcd-vapp.rb -v2 \
   -A --vapptemplate Admin,'Committed Backup - Admin',CBMON -nCBMON  \
-  -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vsp-datastore.xml > /dev/null
+  -l$VCDKIT/logs/vcd-vapp.log -t -m $VCDKIT/conf/mail/vcd-vapp.xml > /dev/null
 
