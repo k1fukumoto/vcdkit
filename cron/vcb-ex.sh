63722e39acbe4d5298c467b79b803a118317d40a
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-10-25 21:20:45 +0900

    Add mail notification support to vcb-ex

--- cron/vcb-ex.sh
+++ cron/vcb-ex.sh
@@ -3,5 +3,5 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib
 export VCDKIT=/opt/vmware/vcdkit
 
-$VCDKIT/vcb-ex.rb --chargeback_db 2 -l$VCDKIT/logs/vcb-ex.log > /dev/null
+$VCDKIT/vcb-ex.rb --chargeback_db 2 -l$VCDKIT/logs/vcb-ex.log -t -m $VCDKIT/conf/mail/vcb-ex.xml > /dev/null
 
