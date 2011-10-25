b48c9287bee9891affae6195757de34430c57850
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-10-25 20:41:19 +0900

    Add pattern for 1.6.1

--- vcb-log.rb
+++ vcb-log.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby
-# -*- coding: utf-8 -*-
 require 'date'
 
 CNTR = {
@@ -8,8 +7,8 @@ CNTR = {
   :warn => 0,
   :info => 0,
   :debug => 0,
-  :apiexception => 0,
-  :stacktrace => 0,
+  :stacktrace_apiexception => 0,
+  :stacktrace_other => 0,
   
 }
 
@@ -17,6 +16,7 @@ ARGV.collect {|f| File.new(f)}.sort {|a,b| a.mtime <=> b.mtime}.each do |file|
   t0 = t1 = nil
   in_ae = false
   n = 1
+  prev_line = nil
   while(line = file.gets)
     if(line =~ /^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/)
       t1 = $1
@@ -28,22 +28,27 @@ ARGV.collect {|f| File.new(f)}.sort {|a,b| a.mtime <=> b.mtime}.each do |file|
     CNTR[:info] += 1 if line =~ /INFO/
     CNTR[:debug] += 1 if line =~ /DEBUG/
     if(line =~ /^\tat/)
-      if(in_ae || line =~ /ChargebackApiExecutor/)
-        CNTR[:apiexception] += 1
+      if(in_ae || 
+         line =~ /ChargebackApiExecutor/ ||
+         prev_line =~ /ChargebackApiFailedException/ )
+        CNTR[:stacktrace_apiexception] += 1
         in_ae = true
       else
-        CNTR[:stacktrace] += 1
+        CNTR[:stacktrace_other] += 1
 #        puts "#{n}:" + line
       end
     else
       in_ae = false
     end
     n += 1
+    prev_line = line
   end
   dt = DateTime.parse(t1) - DateTime.parse(t0)
-  puts sprintf("%-20s: %02d days %02d hrs(%s ~ %s)",file.path,dt, (dt - dt.floor) * 24,t0,t1)
+  hrs = (dt - dt.floor) * 24
+  mins = (hrs - hrs.floor) * 60
+  puts sprintf("%-18s: %02d days %02d hrs(%s ~ %s)",file.path,dt, (dt - dt.floor) * 24,t0,t1)
 end
 
-[:debug,:info,:warn,:error,:apiexception,:stacktrace,:total].each do |type|
+[:debug,:info,:warn,:error,:stacktrace_apiexception,:stacktrace_other,:total].each do |type|
   puts sprintf("%-12s %8d (%d%%)\n",type,CNTR[type],CNTR[type]*100/CNTR[:total])
 end
