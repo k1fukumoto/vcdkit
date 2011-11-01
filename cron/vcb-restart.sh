d345d1797495b9c2751be41da477a2c508c2847f
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-11-01 20:56:44 +0900

    Fix typo for command option

--- cron/vcb-restart.sh
+++ cron/vcb-restart.sh
@@ -3,5 +3,5 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib:/usr/lib/vmware-vix
 export VCDKIT=/opt/vmware/vcdkit
 
-$VCDKIT/vcb-ex.rb --chargeback_db 2 -c5 --restart_vcddb -l$VCDKIT/logs/vcb-ex.log -t -m $VCDKIT/conf/mail/vcb-ex.xml > /dev/null
+$VCDKIT/vcb-ex.rb --chargeback_db 2 -c5 --restart_vcddc -l$VCDKIT/logs/vcb-ex.log -t -m $VCDKIT/conf/mail/vcb-ex.xml > /dev/null
 
