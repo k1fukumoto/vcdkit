f756708a487df3e36c35ce036dfeeb72db65558b
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-11-02 10:11:18 +0900

    Use new style command opts

--- cron/vcb-ex.sh
+++ cron/vcb-ex.sh
@@ -3,5 +3,7 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib
 export VCDKIT=/opt/vmware/vcdkit
 
-$VCDKIT/vcb-ex.rb --chargeback_db 2 -l$VCDKIT/logs/vcb-ex.log -t -m $VCDKIT/conf/mail/vcb-ex.xml > /dev/null
+$VCDKIT/vcb-ex.rb --chargeback_db 2 \
+  --vcddc CGSdhv-868,CGSdhv-869 \
+  -l$VCDKIT/logs/vcb-ex.log -t -m $VCDKIT/conf/mail/vcb-ex.xml > /dev/null
 
